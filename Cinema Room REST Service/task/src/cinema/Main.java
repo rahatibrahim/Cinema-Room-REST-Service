9304c832bbf297337c13ac62f72d1ffaa63cfa6c
Ibrahim Khalil <rahatibrahim22@gmail.com>
2022-06-07 03:39:57 +0600

    The show begins

--- Cinema Room REST Service/task/src/cinema/Main.java
+++ Cinema Room REST Service/task/src/cinema/Main.java
@@ -2,6 +2,92 @@ package cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Seats {
+    int row;
+    int column;
+
+    Seats() {}
+
+    public Seats(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+}
+
+class Room {
+    int total_rows;
+    int total_columns;
+    List<Seats> available_seats = new ArrayList<>();
+
+    Room() {}
+
+    public Room(int total_rows, int total_columns) {
+        this.total_rows = total_rows;
+        this.total_columns = total_columns;
+
+        for (int i = 1; i <= total_rows; i++) {
+            for (int j = 1; j <= total_columns; j++) {
+                available_seats.add(new Seats(i, j));
+            }
+        }
+    }
+
+    public int getTotal_rows() {
+        return total_rows;
+    }
+
+    public void setTotal_rows(int total_rows) {
+        this.total_rows = total_rows;
+    }
+
+    public int getTotal_columns() {
+        return total_columns;
+    }
+
+    public void setTotal_columns(int total_columns) {
+        this.total_columns = total_columns;
+    }
+
+    public List<Seats> getAvailable_seats() {
+        return available_seats;
+    }
+
+    public void setAvailable_seats(List<Seats> available_seats) {
+        this.available_seats = available_seats;
+    }
+}
+
+@RestController
+class Controller {
+    Room Theater1 = new Room(9, 9);
+
+    @GetMapping("/seats")
+    public Room getInformation() {
+        return Theater1;
+    }
+}
 
 @SpringBootApplication
 public class Main {
