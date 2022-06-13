0bf633ea273bfe99b2c61657e9b838fecac1d610
Ibrahim Khalil <rahatibrahim22@gmail.com>
2022-06-13 22:46:47 +0600

    Take your seat

--- Cinema Room REST Service/task/src/cinema/Main.java
+++ Cinema Room REST Service/task/src/cinema/Main.java
@@ -2,21 +2,25 @@ package cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 class Seats {
     int row;
     int column;
+    int price;
 
-    Seats() {}
+    Seats() {
+    }
 
-    public Seats(int row, int column) {
+    public Seats(int row, int column, int price) {
         this.row = row;
         this.column = column;
+        this.price = price;
     }
 
     public int getRow() {
@@ -34,22 +38,35 @@ class Seats {
     public void setColumn(int column) {
         this.column = column;
     }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
 
 class Room {
     int total_rows;
     int total_columns;
-    List<Seats> available_seats = new ArrayList<>();
+    CopyOnWriteArrayList<Seats> available_seats = new CopyOnWriteArrayList<>();
 
-    Room() {}
+    Room() {
+    }
 
     public Room(int total_rows, int total_columns) {
         this.total_rows = total_rows;
         this.total_columns = total_columns;
 
-        for (int i = 1; i <= total_rows; i++) {
-            for (int j = 1; j <= total_columns; j++) {
-                available_seats.add(new Seats(i, j));
+        for (int row = 1; row <= total_rows; row++) {
+            for (int col = 1; col <= total_columns; col++) {
+                int price = 8;
+                if (row <= 4) {
+                    price = 10;
+                }
+                available_seats.add(new Seats(row, col, price));
             }
         }
     }
@@ -70,24 +87,76 @@ class Room {
         this.total_columns = total_columns;
     }
 
-    public List<Seats> getAvailable_seats() {
+    public CopyOnWriteArrayList<Seats> getAvailable_seats() {
         return available_seats;
     }
 
-    public void setAvailable_seats(List<Seats> available_seats) {
+    public void setAvailable_seats(CopyOnWriteArrayList<Seats> available_seats) {
         this.available_seats = available_seats;
+    }
+}
+
+class TicketInfo {
+    Integer row;
+    Integer column;
+
+    TicketInfo() {
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
     }
 }
 
 @RestController
 class Controller {
     Room Theater1 = new Room(9, 9);
+    CopyOnWriteArrayList<Seats> available_seats = Theater1.available_seats;
 
     @GetMapping("/seats")
     public Room getInformation() {
         return Theater1;
     }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<Object> purchaseTicket(@RequestBody TicketInfo tickets) {
+        Integer row = tickets.getRow();
+        Integer column = tickets.getColumn();
+        if (row == null
+                || column == null
+                || row > 9
+                || column > 9
+                || row <= 0
+                || column <= 0) {
+            return new ResponseEntity<>(Map.of(
+                    "error", "The number of a row or a column is out of bounds!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+        for (Seats s : available_seats) {
+            if (s.getRow() == row && s.getColumn() == column) {
+                available_seats.remove(s);
+                return ResponseEntity.ok().body(s);
+            }
+        }
+
+        return new ResponseEntity<>(Map.of(
+                "error", "The ticket has been already purchased!"),
+                HttpStatus.BAD_REQUEST);
+    }
 }
+
 
 @SpringBootApplication
 public class Main {
